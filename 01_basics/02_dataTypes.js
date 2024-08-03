96d1d4ae607cf5a902708f67fede5d46b44bb351
zaidpathan21 <157598303+zaidpathan21@users.noreply.github.com>
2024-08-03 16:49:16 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using nodejs, not browser
+
+console.log(3
+    +
+    3); //code readability should be high
+
+console.log("Zaid");
+
+let name = "Zaid"
+let age = 18
+let isoggedIn = false
+
+// number => 2 to the power of 53
+// bigint 
+// string => ""
+// bollean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqe
+
+
+// object
+
+console.log(typeof undefined); // undefine
+console.log(typeof null); //null
+
